1bc7f6d678c1a9b8ddd7c0b6ccc630ac0e775e1c
AntonShishkin <nuegotonevzlomaut@gmail.com>
2021-03-11 22:19:49 +0300

    create quest_4.py

---
+++ quest_4.py
@@ -0,0 +1,23 @@
+def return_power(a, b):
+    if b == int(b):
+        b = int(b)
+        if b < 0:
+            a1 = a
+            for i in range(abs(b)-1):
+                a1 = a1 * a
+            return 1/a1
+        else:
+            return "Функция должна принимать отрицательное второе число"
+    else:
+        return "Второе число не целочисленное"
+
+
+
+a = input("Введите первое число: ")
+b = input("Введите второе целове отрицательное число: ")
+try:
+    a = float(a)
+    b = float(b)
+    print(f"Результат: {return_power(a, b)}")
+except ValueError:
+    print("Введены не числа")

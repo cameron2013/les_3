fb249787a1039dcdf45956c9c3061c6a3b0b94b4
AntonShishkin <nuegotonevzlomaut@gmail.com>
2021-03-11 21:41:55 +0300

    change quest_2.py

--- venv/quest_2.py
+++ venv/quest_2.py
@@ -1,4 +1,11 @@
-def info_person(name, surname, year, city, email, phone):
+def info_person():
+    name = input("Введите имя: ")
+    surname = input("Введите фамилию: ")
+    year = input("Введите год рождения: ")
+    city = input("Введите город проживания: ")
+    email = input("email: ")
+    phone = input("тел.: ")
     return f"{name} {surname} {year}-го года рождения проживает в городе {city}/-е. Email: {email}, тел.: {phone}"
 
-print(info_person(name=input("Введите имя: "), surname=input("Введите фамилию: "), year=input("Введите год рождения: "), city=input("Введите город проживания: "), email=input("email: "), phone=input("тел.: ")))
+
+print(info_person())

6205a374c30d1e5980e8052d24736253d9b60670
AntonShishkin <nuegotonevzlomaut@gmail.com>
2021-03-11 21:56:19 +0300

    create quest_3.py

---
+++ quest_3.py
@@ -0,0 +1,13 @@
+def max_sum(a, b, c):
+    return a + b + c - min(a, b, c)
+
+a = input("Введите а: ")
+b = input("Введите b: ")
+c = input("Введите c: ")
+try:
+    a = float(a)
+    b = float(b)
+    c = float(c)
+    print(f"Максимальная сумма двух элементов: {max_sum(a, b, c)}")
+except ValueError:
+    print("Переменные не числа!")
